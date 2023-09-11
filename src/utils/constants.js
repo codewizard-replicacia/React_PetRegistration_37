86e7f11ffc81c74f1135d96dbea39f817af0d9e8
Administrator <Administrator@172.31.36.201>
2023-09-11 10:05:51 +0000

    initial commit

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const BASE_URL = "$frontendApp.getNamespace()"; //Temporarily using field; Enter Backend URL here e.g. http://3.133.64.67:8164/generated_app
+export const PATH_PETOWNER = "/PetOwners";
+export const PATH_VETERINARYDOCTOR = "/VeterinaryDoctors";
+export const PATH_VACCINE = "/Vaccines";
+export const PATH_PETVISIT = "/PetVisits";
+export const PATH_PET = "/Pets";
---
+++ src/utils/display_configuration.js
@@ -0,0 +1,48 @@
+export const petViewConfig = {
+Details: [
+    {
+      key: "PetId",
+      value: "PetId",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "PetName",
+      value: "PetName",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Breed",
+      value: "Breed",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Weight",
+      value: "Weight",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "IsVaccinated",
+      value: "IsVaccinated",
+      type: "boolean",
+      editable: false,
+      required: true
+    },
+    {
+      key: "VaccineDueDate",
+      value: "VaccineDueDate",
+      type: "date",
+      editable: false,
+      required: true
+    },
+  ],
+Type: [
+  ],
+}
